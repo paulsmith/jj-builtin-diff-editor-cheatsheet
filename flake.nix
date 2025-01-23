5549137aae0c8a1d6062a6208d5a1309474fdade
Paul Smith <paulsmith@gmail.com>
2025-01-23 11:01:28 -0600

    Add Nix flake (for formatter)

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Cheatsheet for jj's builtin diff/merge tool";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        fn:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.prettier
+          ];
+        };
+      });
+    };
+}
